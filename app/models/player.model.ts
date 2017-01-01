9bfc89659b13328a35ce84a5900f0fd2b7ba7b64
Jose Almada <jose_almada@hotmail.com>
2017-01-01 12:49:55 -0500

    Update Participants basic behavior

--- app/models/player.model.ts
+++ app/models/player.model.ts
@@ -1,6 +1,8 @@
 export class Player
 {
-    
+    private _hideEditMode : boolean = true;
+    private _nameView : string = '';
+
 
     constructor (private _id : number, private _name : string){ }
 
@@ -9,8 +11,34 @@ export class Player
         return this._name;
     }
 
-     get Id()
+    set NameView(value)
+    {
+        this._nameView = value;
+    }
+
+    get Id()
     {
         return this._id;
+    }
+
+    get HideEditMode()
+    {
+        return this._hideEditMode;
+    }
+
+    set HideEditMode(value)
+    {
+        this._hideEditMode = value;
+    }
+
+    edit()
+    {
+        this._hideEditMode = false;
+    }
+
+    save()
+    {
+        this._name = this._nameView;
+        this._hideEditMode = true;
     }
 }
